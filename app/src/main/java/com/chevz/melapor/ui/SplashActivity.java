c8ce6515f5b6d20444ab1d7468d7655e181faeee
Chevz88 <chevz.hanavimichi@gmail.com>
2025-06-17 17:44:04 +0700

    Update SplashActivity.java

--- app/src/main/java/com/chevz/melapor/ui/SplashActivity.java
+++ app/src/main/java/com/chevz/melapor/ui/SplashActivity.java
@@ -4,25 +4,18 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.WindowManager;
 import com.chevz.melapor.R;
 
 public class SplashActivity extends Activity {
-
-    private static final int SPLASH_TIMEOUT = 3000; // durasi 3 detik
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // fullscreen tanpa status bar
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                             WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
 
-        // Delay sebelum pindah ke LoginActivity
+        // Delay splash selama 2 detik lalu ke LoginActivity
         new Handler().postDelayed(() -> {
             startActivity(new Intent(SplashActivity.this, LoginActivity.class));
-            finish(); // tutup splash agar tidak bisa di-back
-        }, SPLASH_TIMEOUT);
+            finish(); // supaya tidak bisa kembali ke splash
+        }, 2000);
     }
 }
