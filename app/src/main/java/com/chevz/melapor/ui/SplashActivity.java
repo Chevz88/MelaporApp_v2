2a096857f46bca123c9b23feddc9c7568b6374e0
Chevz88 <chevz.hanavimichi@gmail.com>
2025-06-17 20:26:50 +0700

    Update SplashActivity.java

--- app/src/main/java/com/chevz/melapor/ui/SplashActivity.java
+++ app/src/main/java/com/chevz/melapor/ui/SplashActivity.java
@@ -1,20 +1,24 @@
 package com.chevz.melapor.ui;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 import com.chevz.melapor.R;
 
 public class SplashActivity extends Activity {
+    private static final int SPLASH_TIME_OUT = 2000; // 2 detik
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
         // Delay splash selama 2 detik lalu ke LoginActivity
-        new Handler().postDelayed(() -> {
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
             startActivity(new Intent(SplashActivity.this, LoginActivity.class));
             finish(); // supaya tidak bisa kembali ke splash
-        }, 2000);
+        }, SPLASH_TIME_OUT);
     }
 }
